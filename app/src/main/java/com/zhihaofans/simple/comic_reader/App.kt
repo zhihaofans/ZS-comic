5c55d73184e1ec93bd2798d75198e7f8981b9951
zhihaofans <zhihaofans@hotmail.com>
2018-07-13 21:52:38 +0800

    Add init to App.kt

--- app/src/main/java/com/zhihaofans/simple/comic_reader/App.kt
+++ app/src/main/java/com/zhihaofans/simple/comic_reader/App.kt
@@ -1,7 +1,17 @@
 package com.zhihaofans.simple.comic_reader
 
 import android.app.Application
+import com.haoge.easyandroid.EasyAndroid
+import com.orhanobut.logger.AndroidLogAdapter
+import com.orhanobut.logger.Logger
+import com.maning.librarycrashmonitor.MCrashMonitor
+
 
 class App : Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        Logger.addLogAdapter(AndroidLogAdapter())
+        MCrashMonitor.init(this, true) { file -> Logger.e("CrashMonitor回调:" + file.absolutePath) }
+        EasyAndroid.init(this)
+    }
 }
